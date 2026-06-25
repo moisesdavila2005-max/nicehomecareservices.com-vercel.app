8e888f71363452ddaf17e1d4ff8a76c09521e888
moisesdavila2005-max <moisesdavila2005@gmail.com>
2026-06-25 04:41:38 -0600

    Update layout.tsx

--- layout.tsx
+++ layout.tsx
@@ -32,7 +32,7 @@ export default function RootLayout({
 }>) {
   return (
     <html
-      lang="es"
+      lang="es | en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">
