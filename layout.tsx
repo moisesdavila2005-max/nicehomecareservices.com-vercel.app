8fb7bffaeb0a64745f8bcf030d506035cffda397
moisesdavila2005-max <moisesdavila2005@gmail.com>
2026-06-24 21:14:09 -0600

    Actualizar layout.tsx

--- layout.tsx
+++ layout.tsx
@@ -48,7 +48,7 @@ export default function RootLayout({
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
-            gtag('config', 'G-XXXXXXXXXX', {
+            gtag('config', 'GTM-GT4PSJZR', {
               page_path: window.location.pathname,
             });
           `}
